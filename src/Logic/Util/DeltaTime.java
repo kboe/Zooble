d9dff2009f4f2d3ff7f29f6ccf6a62722bce83b3
kboe <karen.boerner@stud.hshl.de>
2017-05-21 20:42:19 +0200

    Getter und Setter für Delta Time

--- src/Logic/Util/DeltaTime.java
+++ src/Logic/Util/DeltaTime.java
@@ -5,6 +5,18 @@ package Logic.Util;
  */
 public class DeltaTime {
 
-    double deltatime = 0.016667;
+    public static double deltatime = 0.016667;
+    private double currentTime = 0.01667;
 
+    public double getDeltatime() {
+        return deltatime;
+    }
+
+    public double getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(double currentTime) {
+        this.currentTime = currentTime;
+    }
 }
