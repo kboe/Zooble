f5ee2e83533ff8403c5a1d87fece96050c6a0c4c
kboe <karen.boerner@stud.hshl.de>
2017-05-22 11:02:15 +0200

    Tidying up in Physics
    Signed-off-by: kboe <karen.boerner@stud.hshl.de>

--- src/Logic/Util/Physics/Physics.java
+++ src/Logic/Util/Physics/Physics.java
@@ -1,21 +1,19 @@
-package Logic;
+package Logic.Util.Physics;
 
 
 /**
- * Created by Administrator on 14.05.2017.
+ * Created by Karen on 14.05.2017.
  */
 public class Physics {
 
+    private double lastFrameTime; //time Stamp
+    private double s0;
+    private double s1 = 1;
 
-    long t0;
-    long t1;
-    double s0;
-    double s1 = 1;
-    int time;
 
-    double v;
-    public final double gravity = 9.81;
-    double y = 100;
+    private double v; //velocity
+    public final double gravity = 9.81; //gravity
+    private double y = 100;
 
     boolean collided = false;
 
@@ -40,23 +38,24 @@ public class Physics {
     }
 
     /**
-     * Gleichmäßig beschleunigte Bewegung
-     * Geschwindigkeit
+     * Position with consistent velocity
      *
-     * @param v
-     * @param a
-     * @param t
+     * @param v velocity
+     * @param a acceleration
+     * @param t time
+     * @return current position
      */
-    public void setV(double v, double a, int t) {
-        this.v = a * t + v;
+    public double consistentVelocity(double v, double a, int t) {
+        double pos = a * t + v;
+        return pos;
     }
 
     /**
-     * Freier Fall Position
+     * Free Fall from certain height
      *
-     * @param h
-     * @param time
-     * @return
+     * @param h    y-Position
+     * @param time time
+     * @return current y Position
      */
     public double yPos(double h, double time) {
         double y = getY() + gravity / 2 * (time * time);
@@ -66,31 +65,22 @@ public class Physics {
     }
 
 
-    int radius = 50;
-
-    public int getTime() {
-        return time;
+    /**
+     * Get time from last Frame
+     *
+     * @return time of last Frame
+     */
+    public double getLastFrameTime() {
+        return lastFrameTime;
     }
 
-    public void setTime(int time) {
-        this.time = time;
-    }
-
-    public long getT0() {
-        return t0;
-    }
-
-    public void setT0(long t0) {
-        this.t0 = t0;
-    }
-
-    public long getT1() {
-        return t1;
-    }
-
-    public void setT1(long t1) {
-        System.out.println(t1);
-        this.t1 = t1;
+    /**
+     * Set current time for next frame
+     *
+     * @param lastFrameTime
+     */
+    public void setLastFrameTime(double lastFrameTime) {
+        this.lastFrameTime = lastFrameTime;
     }
 
     public double getS0() {
@@ -109,19 +99,31 @@ public class Physics {
         this.s1 = s1;
     }
 
-
-    public double xSpeed(long t0, long t1, double s0, double s1) {
-        double v = (s1 - s0) / (t1 - t0);
-        System.out.println(v);
+    //TODO doesnt really make sense yet
+    /**
+     * Calculate instaneous Velocity
+     *
+     * @param lastFrameTime
+     * @param currentTime
+     * @param lastPos
+     * @param currentPos
+     * @return velocity
+     */
+    public double instantaneousVelocity(double lastFrameTime, double currentTime, double lastPos, double currentPos) {
+        double v = (currentPos - lastPos) / (currentTime - lastFrameTime);
         return v;
     }
 
-    public double xPos(double v, double t, double s0) {
-        double s = (v * t + s0);
-        //setS0(s);
-        setY(s);
-        System.out.println(s);
-        return s;
+    /**
+     * Calculate current position with consistent velocity
+     * @param velocity
+     * @param time
+     * @param lastPosition
+     * @return current position
+     */
+    public double linearEvenlyMoving(double velocity, double time, double lastPosition) {
+        double position = (velocity*time)+lastPosition;
+        return position;
     }
 
     public double freeFallSpeed(int time, double gravity) {
@@ -131,37 +133,41 @@ public class Physics {
 
     /**
      * Für erste Kugel die Geschwindigkeit bei Kugelkollision
+     *
      * @param m1
      * @param m2
      * @param v1
      * @param v2
      * @return
      */
-    public double elStv0(double m1, double m2, double v1, double v2){
-        double u1= ((m1-m2)*(v1*v1)+(2*m2*v2))/(m1+m2);
-        return  u1;
+    public double elStv0(double m1, double m2, double v1, double v2) {
+        double u1 = ((m1 - m2) * (v1 * v1) + (2 * m2 * v2)) / (m1 + m2);
+        return u1;
     }
+
     /**
      * Für zweite Kugel die Geschwindigkeit bei Kugelkollision
+     *
      * @param m1
      * @param m2
      * @param v1
      * @param v2
      * @return
      */
-    public double elStv1(double m1, double m2, double v1, double v2){
-        double u2= ((m2-m1)*(v2*v2)+(2*m1*v1))/(m1+m2);
-        return  u2;
+    public double elStv1(double m1, double m2, double v1, double v2) {
+        double u2 = ((m2 - m1) * (v2 * v2) + (2 * m1 * v1)) / (m1 + m2);
+        return u2;
     }
 
     /**
      * Kinetische Energie
+     *
      * @param m
      * @param v
      * @return
      */
-    public double eKin(double m, double v){
-        double eKin = 0.5*m*(v*v);
+    public double eKin(double m, double v) {
+        double eKin = 0.5 * m * (v * v);
         return eKin;
     }
 
@@ -209,21 +215,23 @@ public class Physics {
 
     /**
      * Konstante Beschleunigung
+     *
      * @param dv
      * @param dt
      * @return
      */
     public double cAcceleration(double dv, double dt) {
-        double a = dv/dt;
+        double a = dv / dt;
         return 0;
     }
 
-    public double wX(double v0,double time){
-        double x= v0*time;
+    public double wX(double v0, double time) {
+        double x = v0 * time;
         return x;
     }
-    public double wY(double gravity, double time){
-        double y = gravity*(time*time);
+
+    public double wY(double gravity, double time) {
+        double y = gravity * (time * time);
         setY(y);
         return y;
     }
@@ -231,13 +239,14 @@ public class Physics {
 
     /**
      * Konstante Beschleunigung
+     *
      * @param dv
      * @param t0
      * @param t1
      * @return
      */
     public double cAcceleration(double dv, double t0, double t1) {
-        double a = dv/(t1-t0);
+        double a = dv / (t1 - t0);
         return a;
     }
 
