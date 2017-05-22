c7753467a46d60cf56962bbcf62389d17cc5931a
kboe <karen.boerner@stud.hshl.de>
2017-05-22 19:06:47 +0200

    Tidying up in Physics
    Signed-off-by: kboe <karen.boerner@stud.hshl.de>

--- src/Logic/Util/Physics/UltraPhysics.java
+++ src/Logic/Util/Physics/UltraPhysics.java
@@ -1,7 +1,9 @@
 package Logic.Util.Physics;
 
+import Logic.Collision.CircleCollider;
 import Logic.Collision.Collider;
 import Logic.Util.DeltaTime;
+import javafx.scene.shape.Circle;
 
 import static Logic.Util.Physics.Physics.GRAVITY;
 
@@ -14,8 +16,20 @@ public class UltraPhysics {
      *
      * @param collider
      */
-    public static void freeFall(Collider collider,DeltaTime deltaTime) {
+    public static void freeFall(CircleCollider collider, DeltaTime deltaTime) {
         collider.getPositionVector().setY(collider.getPositionVector().getY() + (GRAVITY / 2 * (deltaTime.getCurrentTime()* deltaTime.getCurrentTime())));
+    }
+
+
+    public static void consistentVelocity(CircleCollider collider,double v, double a, DeltaTime deltaTime) {
+        double pos = a * deltaTime.getCurrentTime() + v;
+        collider.getPositionVector().setX(collider.getPositionVector().getX() + pos);
+
+    }
+
+    public static double move(Collider collider, double v, DeltaTime deltaTime){
+        double p = v*deltaTime.getCurrentTime();
+        return p;
     }
 
 }
