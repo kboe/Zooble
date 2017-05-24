8a46a9c3632a779b510570bd3e6f4c11d9757a11
kboe <karen.boerner@stud.hshl.de>
2017-05-24 15:55:26 +0200

    Kinematics
    Signed-off-by: kboe <karen.boerner@stud.hshl.de>

--- src/Logic/Util/Physics/Kinematics.java
+++ src/Logic/Util/Physics/Kinematics.java
@@ -14,47 +14,97 @@ public class Kinematics {
 
     /**
      * Calculates speed with acceleration
+     *
      * @param a
      * @param deltaTime
      * @return
      */
-    public static double acceleratedMovementVelocity(double a, DeltaTime deltaTime){
-        double velocity = a*deltaTime.getCurrentTime();
+    public static double acceleratedMovementVelocity(double a, DeltaTime deltaTime) {
+        double velocity = a * deltaTime.getCurrentTime();
         return velocity;
     }
 
-    public static double accleratedMovementPosition(double acceleration, DeltaTime deltaTime){
-        double s = 0.5*acceleration*(deltaTime.getCurrentTime()*deltaTime.getCurrentTime());
-        return 0;
+    /**
+     * Calculates current position with time and acceleration
+     *
+     * @param acceleration
+     * @param deltaTime
+     * @return
+     */
+    public static double accleratedMovementPosition(double acceleration, DeltaTime deltaTime) {
+        double s = 0.5 * acceleration * (deltaTime.getCurrentTime() * deltaTime.getCurrentTime());
+        return s;
     }
 
     //----------------------------------------------------------------------------------------------------------------
     //Beschleunigte Bewegung mit Anfangsgeschwindigkeit
 
     /**
-     * Position with consistent velocity with beginning velocity
+     * Calculates speed with acceleration and starting velocity
      *
-     * @param v velocity
-     * @param a acceleration
-     * @param t time
-     * @return current position
+     * @param a
+     * @param deltaTime
+     * @return
      */
-    public static double consistentVelocity(double v, double a, int t) {
-        double pos = a * t + v;
-        return pos;
+    public static double acceleratedMovementVelocityWithStartingVelocity(double a, DeltaTime deltaTime, double velocity0) {
+        double velocity = a * deltaTime.getCurrentTime() + velocity0;
+        return velocity;
+    }
+
+    /**
+     * Calculates current position with starting position and starting velocity
+     *
+     * @param position0
+     * @param velocity0
+     * @param deltaTime
+     * @param acceleration
+     * @return
+     */
+    public static double acceleratedMovementPositionWithStartingSpeedAndPosition(double position0, double velocity0, DeltaTime deltaTime, double acceleration) {
+        double position = position0 + velocity0 * deltaTime.getCurrentTime() + 0.5 * acceleration * (deltaTime.getCurrentTime() * deltaTime.getCurrentTime());
+        return position;
     }
 
     //----------------------------------------------------------------------------------------------------------------
     //Durchschnittsgeschwindigkeit
 
+    /**
+     * Mean speed
+     *
+     * @param pos0
+     * @param pos1
+     * @param deltaTime0
+     * @param deltaTime1
+     * @return
+     */
+    public static double effectiveSpeed(double pos0, double pos1, DeltaTime deltaTime0, DeltaTime deltaTime1) {
+        double velocity = ((pos1 - pos0) / (deltaTime1.getCurrentTime() - deltaTime0.getCurrentTime()));
+        return velocity;
+    }
+
     //----------------------------------------------------------------------------------------------------------------
     //Durchschnittsbeschleunigung
+
+    /**
+     * Mean acceleration
+     *
+     * @param velocity0
+     * @param velocity1
+     * @param deltaTime0
+     * @param deltaTime1
+     * @return
+     */
+    public static double effectiveAcceleration(double velocity0, double velocity1, DeltaTime deltaTime0, DeltaTime deltaTime1) {
+        double acceleration = (velocity1 - velocity0) / (deltaTime1.getCurrentTime() - deltaTime0.getCurrentTime());
+        return 0;
+    }
 
     //----------------------------------------------------------------------------------------------------------------
     //Freier Fall
 
     /**
      * Free Fall y-Pos
+     *
      * @param deltaTime
      * @return
      */
@@ -65,11 +115,25 @@ public class Kinematics {
 
     /**
      * Calculate Free Fall speed from height and gravity
+     *
      * @param h
      * @return
      */
-    public static double freeFallVelocity(double h){
-        double velocity = Math.sqrt(2*h*GRAVITY);
+    public static double freeFallVelocity(double h) {
+        double velocity = Math.sqrt(2 * h * GRAVITY);
         return velocity;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------
+    //Waagrechter Wurf
+
+    public static double levelThrowXPos(double velocityX, DeltaTime deltaTime) {
+        double xPos = velocityX * deltaTime.getCurrentTime();
+        return xPos;
+    }
+
+    public static double levelThrowYPos(double height, DeltaTime deltaTime) {
+        double yPos = height - 0.5 * GRAVITY * (deltaTime.getCurrentTime() * deltaTime.getCurrentTime());
+        return yPos;
     }
 }
