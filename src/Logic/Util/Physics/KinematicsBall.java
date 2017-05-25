9622d0001559e4eff7758d5e596ee86a41b07afe
kboe <karen.boerner@stud.hshl.de>
2017-05-25 18:25:30 +0200

    Kinematics for Ball
    Signed-off-by: kboe <karen.boerner@stud.hshl.de>

--- src/Logic/Util/Physics/KinematicsBall.java
+++ src/Logic/Util/Physics/KinematicsBall.java
@@ -10,13 +10,12 @@ import Logic.Util.Vector2d;
 public class KinematicsBall {
 
 
-
-
     //---------------------------------------------------------------------------------
     //Gradlining beschleunigte Bewegung
 
     /**
      * Sets position with with velocity, time and position
+     *
      * @param velocity
      * @param deltaTime
      * @param bc
@@ -36,9 +35,9 @@ public class KinematicsBall {
      * @param deltaTime
      * @return
      */
-    public static void acceleratedMovementVelocity(double a, DeltaTime deltaTime,BallCollider bc) {
+    public static void acceleratedMovementVelocity(double a, DeltaTime deltaTime, BallCollider bc) {
         double velocity = a * deltaTime.getCurrentTime();
-        bc.setCurrentVelocity(new Vector2d(velocity,0));
+        bc.setCurrentVelocity(new Vector2d(velocity, bc.getVelocity().getY()));
         //bc.setLayoutX(velocity);
     }
 
@@ -49,9 +48,39 @@ public class KinematicsBall {
      * @param deltaTime
      * @return
      */
-    public static double accleratedMovementPosition(double acceleration, DeltaTime deltaTime) {
+    public static void accleratedMovementPosition(double acceleration, DeltaTime deltaTime, BallCollider bc) {
         double s = 0.5 * acceleration * (deltaTime.getCurrentTime() * deltaTime.getCurrentTime());
-        return s;
+        bc.setPosition(new Vector2d(s, bc.getPosition().getY()));
     }
+
+
+    //----------------------------------------------------------------------------------------------------------------
+    //Beschleunigte Bewegung mit Anfangsgeschwindigkeit
+
+    /**
+     * Calculates speed with acceleration and starting velocity
+     *
+     * @param a
+     * @param deltaTime
+     * @return
+     */
+    public static void acceleratedMovementVelocityWithStartingVelocity(double a, DeltaTime deltaTime, BallCollider bc) {
+        double velocity = a * deltaTime.getCurrentTime() + bc.getLastVelocity().getX();
+
+        bc.setVelocity(new Vector2d(velocity, bc.getVelocity().getY()));
+    }
+
+
+    /**
+     * Calculates current position with starting position and starting velocity
+     * @param bc
+     * @param deltaTime
+     * @param acceleration
+     */
+    public static void acceleratedMovementPositionWithStartingSpeedAndPosition(BallCollider bc, DeltaTime deltaTime, double acceleration) {
+        double position = bc.getPosition().getX() + bc.getLastVelocity().getX()* deltaTime.getCurrentTime() + 0.5 * acceleration * (deltaTime.getCurrentTime() * deltaTime.getCurrentTime());
+        bc.setVelocity(new Vector2d(position,bc.getPosition().getY()));
+    }
+
 
 }
